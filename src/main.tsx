e6a1bfe4aba31ff68b70564c4ff9822407e80dc3
ihyeeun <oioigl@naver.com>
2025-10-28 20:16:47 +0900

    feat: Tanstack Query setting

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import App from "./App.tsx";
 import "./index.css";
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools />
+      <App />
+    </QueryClientProvider>
   </BrowserRouter>,
 );
