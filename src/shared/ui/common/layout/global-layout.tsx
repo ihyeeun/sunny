ab80da5806be21de67f381d76a2a35feaec0d488
ihyeeun <oioigl@naver.com>
2025-10-29 17:26:40 +0900

    feat: global main, footer 구현

--- src/shared/ui/common/layout/global-layout.tsx
+++ src/shared/ui/common/layout/global-layout.tsx
@@ -1,16 +1,17 @@
 import { Outlet } from "react-router";
 
+import GlobalFooter from "./global-footer";
 import GlobalHeader from "./global-header";
 
 export default function GlobalLayout() {
   return (
-    <div>
+    <div className="flex min-h-screen flex-col">
       {/* <h1 className="visuallyhidden">Sunny 한국</h1> */}
       <GlobalHeader />
-      <main>
+      <main className="layout-container flex-1 border-x py-6">
         <Outlet />
       </main>
-      <footer>푸터</footer>
+      <GlobalFooter />
     </div>
   );
 }
