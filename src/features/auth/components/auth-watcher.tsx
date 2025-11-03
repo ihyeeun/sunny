ac27485d20ae467fce003f5a20dbe5236d879802
ihyeeun <oioigl@naver.com>
2025-11-04 01:40:06 +0900

    feat: 회원가입된 유저 프로필을 자동으로 생성해주는 기능 구현
    첫 로그인시 프로필이 없는 회원들은 요청을 가로채서 임의 프로필을 만들어주는 작업을 진행하였음.

--- src/features/auth/components/auth-watcher.tsx
+++ src/features/auth/components/auth-watcher.tsx
@@ -1,12 +1,22 @@
 import { type ReactNode, useEffect } from "react";
 
 import supabase from "@shared/lib/supabase";
-import { useSessionLoadedState, useSetSession } from "@shared/store/session";
+import {
+  useSessionLoadedState,
+  useSessionState,
+  useSetSession,
+} from "@shared/store/session";
 import { GlobalLoaded } from "@shared/ui/common";
+import { useProfileDataQuery } from "@features/user/hooks/queries/use-profile-data-query";
 
 export default function AuthWatcher({ children }: { children: ReactNode }) {
+  const sessionState = useSessionState();
   const setSession = useSetSession();
   const hasSessionLoaded = useSessionLoadedState();
+
+  const { data: profile, isLoading: isProfileLoading } = useProfileDataQuery(
+    sessionState?.user.id,
+  );
 
   useEffect(() => {
     supabase.auth.onAuthStateChange((event, session) => {
@@ -15,6 +25,7 @@ export default function AuthWatcher({ children }: { children: ReactNode }) {
   });
 
   if (!hasSessionLoaded) return <GlobalLoaded />;
+  if (isProfileLoading) return <GlobalLoaded />;
 
   return children;
 }
---
+++ src/features/user/hooks/queries/use-profile-data-query.ts
@@ -0,0 +1,28 @@
+import type { PostgrestError } from "@supabase/supabase-js";
+import { useQuery } from "@tanstack/react-query";
+
+import { useSessionState } from "@shared/store/session";
+import { getProfile } from "@features/user/api/get-profile";
+import { postCreateProfile } from "@features/user/api/post-create-profile";
+import { PROFILE_QUERY_KEYS } from "@features/user/constants/query-key";
+
+export function useProfileDataQuery(userId?: string) {
+  const session = useSessionState();
+  const isMine = userId === session?.user.id;
+  return useQuery({
+    queryFn: async () => {
+      try {
+        const profile = await getProfile(userId!);
+        return profile;
+      } catch (error) {
+        if (isMine && (error as PostgrestError).code === "PGRST116") {
+          // 남의 프로필이 없을 때 생성하지 않기 하기 위함으로 내 프로필일때 조건을 넣은 것.
+          return await postCreateProfile(userId!);
+        }
+        throw error;
+      }
+    },
+    queryKey: PROFILE_QUERY_KEYS.profile.byId(userId!),
+    enabled: !!userId,
+  });
+}
