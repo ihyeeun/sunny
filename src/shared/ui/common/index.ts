d1b0187782fa56eed9462779728b2fc15adc84f6
ihyeeun <oioigl@naver.com>
2025-12-03 16:42:00 +0900

    feat: global fallback 구현

--- src/shared/ui/common/index.ts
+++ src/shared/ui/common/index.ts
@@ -1,3 +1,4 @@
+export { default as Fallback } from "./layout/fallback";
 export { default as GlobalFooter } from "./layout/global-footer";
 export { default as GlobalHeader } from "./layout/global-header";
 export { default as GlobalLayout } from "./layout/global-layout";
