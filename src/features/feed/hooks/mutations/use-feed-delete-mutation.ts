5611a4bb8f63ec4b6321a9bdf2acf069eeb4345f
ihyeeun <oioigl@naver.com>
2025-12-04 11:34:34 +0900

    feat: 피드 추가, 수정, 삭제시에 캐시를 적절하게 수정하도록 구현

--- src/features/feed/hooks/mutations/use-feed-delete-mutation.ts
+++ src/features/feed/hooks/mutations/use-feed-delete-mutation.ts
@@ -1,9 +1,12 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 import type { UseMutationCallback } from "@shared/types/callbacks.types";
 import { deleteFeed, deleteImagesInPath } from "@features/feed/api/delete-feed";
+import { FEED_QUERY_KEYS } from "@features/feed/constants/query-key";
 
 export function useFeedDeleteMutation(callbacks?: UseMutationCallback) {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: deleteFeed,
     onSuccess: async (deleteFeed) => {
@@ -11,6 +14,9 @@ export function useFeedDeleteMutation(callbacks?: UseMutationCallback) {
       if (deleteFeed.image_urls && deleteFeed.image_urls.length > 0) {
         await deleteImagesInPath(`${deleteFeed.author_id}/${deleteFeed.id}`);
       }
+      queryClient.resetQueries({
+        queryKey: FEED_QUERY_KEYS.feed.list,
+      });
     },
     onError: (error) => {
       if (callbacks?.onError) return callbacks.onError(error);
