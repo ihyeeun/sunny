730cfb0eae9bf913b01b65de2e7cbf72dd4b5748
ihyeeun <oioigl@naver.com>
2025-12-08 20:43:47 +0900

    fix: vercel analytics 삭제

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { Analytics } from "@vercel/analytics/next";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import { Toaster } from "sonner";
@@ -24,7 +23,6 @@ const queryClient = new QueryClient({
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <QueryClientProvider client={queryClient}>
-      <Analytics />
       <ReactQueryDevtools />
       <Toaster />
       <Modals />
