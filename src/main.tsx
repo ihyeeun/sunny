9d6f0470ab769aac7ed10abb361f409dfa1fe071
ihyeeun <oioigl@naver.com>
2025-12-08 20:40:29 +0900

    feat: ayalytics 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Analytics } from "@vercel/analytics/next";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import { Toaster } from "sonner";
@@ -23,6 +24,7 @@ const queryClient = new QueryClient({
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <QueryClientProvider client={queryClient}>
+      <Analytics />
       <ReactQueryDevtools />
       <Toaster />
       <Modals />
