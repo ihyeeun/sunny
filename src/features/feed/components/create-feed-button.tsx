c6d311e73743941ad3ebab44bd421e6bed5ce854
ihyeeun <oioigl@naver.com>
2025-12-09 23:27:40 +0900

    feat: 글쓰기 버튼 플로팅으로 변경

--- src/features/feed/components/create-feed-button.tsx
+++ src/features/feed/components/create-feed-button.tsx
@@ -1,16 +1,19 @@
-import { PlusCircle } from "lucide-react";
+import { Plus } from "lucide-react";
 
+import { Button } from "@shared/ui/shadcn";
 import { useOpenFeedEditorModal } from "@features/feed/store/feed-editor-modal";
 
 export default function CreateFeedButton() {
   const modalOpen = useOpenFeedEditorModal();
 
   return (
-    <div onClick={modalOpen} className="message-prompt cursor-pointer">
-      <p className="text-muted-foreground text-sm">
-        나누고 싶은 이야기가 있나요?
-      </p>
-      <PlusCircle className="text-muted-foreground size-5" strokeWidth={1} />
-    </div>
+    <Button
+      size="default"
+      onClick={modalOpen}
+      className="fixed right-4 bottom-6 z-50 cursor-pointer rounded-4xl shadow-lg"
+    >
+      <Plus />
+      글쓰기
+    </Button>
   );
 }
