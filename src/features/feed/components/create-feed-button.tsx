f39cfd59d98c4973e71a2b153b53add652a42a79
ihyeeun <oioigl@naver.com>
2025-12-10 23:26:21 +0900

    refactor: 레이아웃에 맞게 로딩창, 플로팅 버튼 설정할 수 있도록 설정

--- src/features/feed/components/create-feed-button.tsx
+++ src/features/feed/components/create-feed-button.tsx
@@ -7,13 +7,17 @@ export default function CreateFeedButton() {
   const modalOpen = useOpenFeedEditorModal();
 
   return (
-    <Button
-      size="default"
-      onClick={modalOpen}
-      className="fixed right-4 bottom-6 z-50 cursor-pointer rounded-4xl shadow-lg"
-    >
-      <Plus />
-      글쓰기
-    </Button>
+    <div className="pointer-events-none fixed inset-x-0 bottom-6 z-50 border">
+      <div className="layout-container flex justify-end">
+        <Button
+          size="default"
+          onClick={modalOpen}
+          className="pointer-events-auto cursor-pointer rounded-4xl shadow-lg"
+        >
+          <Plus />
+          글쓰기
+        </Button>
+      </div>
+    </div>
   );
 }
